275d48cedc5a3af0246e612e88b772d6611edd9b
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-02-23 15:57:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -473,7 +473,7 @@ async def buy_vip(message):
 	except Exception as e:
 		warning_log.warning(e)
 
-
+@dp.message_handler(lambda message: message)
 @dp.message_handler(lambda message: message.text == '👑 VIP per hari')
 async def buy_day(message):
 	try:
@@ -499,6 +499,23 @@ async def buy_week(message):
 	except Exception as e:
 		warning_log.warning(e)
 
+@dp.message_handler(lambda message: message)
+async def buying_week(message):
+
+	try:
+		await bot.send_message(int(message.text), f'Durasi VIP berhasil ditambahkan 7 hari')
+		await bot.send_message(5458705482, f'Durasi VIP berhasil {message.text} ditambahkan 1 hari')
+		if db.get_vip_ends(int(message.text))[0] is None:
+			db.edit_vip_ends((datetime.now() + timedelta(days=1)).strftime('%d.%m.%Y %H:%M'), int(message.text))
+           
+		else:
+			db.edit_vip_ends(
+				(datetime.strptime(db.get_vip_ends(int(message.text))[0], '%d.%m.%Y %H:%M') +
+				 timedelta(days=7)).strftime('%d.%m.%Y %H:%M'), message.text)
+
+	except Exception as e:
+		warning_log.warning(e)
+
 
 @dp.message_handler(lambda message: message.text == '👑 VIP per bulan')
 async def buy_mounth(message):
@@ -511,9 +528,24 @@ async def buy_mounth(message):
 			await message.answer(f'Contact @nazhak\nPrice 25K COIN ONS')
 	except Exception as e:
 		warning_log.warning(e)
+  
+@dp.message_handler(lambda message: message)
+async def buying_mounth(message):
 
+	try:
+		await bot.send_message(int(message.text), f'Durasi VIP berhasil ditambahkan 31 hari')
+		await bot.send_message(5458705482, f'Durasi VIP berhasil ditambahkan 31 hari')
+		if db.get_vip_ends(int(message.text))[0] is None:
+			db.edit_vip_ends((datetime.now() + timedelta(days=31)).strftime('%d.%m.%Y %H:%M'), int(message.text))
+           
+		else:
+			db.edit_vip_ends(
+				(datetime.strptime(db.get_vip_ends(int(message.text))[0], '%d.%m.%Y %H:%M') +
+				 timedelta(days=31)).strftime('%d.%m.%Y %H:%M'), message.text)
+
+	except Exception as e:
+		warning_log.warning(e)
 # Поиск
-
 
 @dp.message_handler(commands=['cancel_search'])
 @dp.message_handler(lambda message: message.text == '🚫 Batalkan pencarian')
