07840c728ae0546048980c22eb671c51b8856c08
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-02-23 13:24:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -491,7 +491,7 @@ async def buying_dayy(message):
 
 	try:
 		await bot.send_message(int(message.text), f'Durasi VIP berhasil ditambahkan 1 hari')
-		await bot.send_message(config.ADMINS, f'Durasi VIP berhasil ditambahkan 1 hari')
+		await bot.send_message(5458705482, f'Durasi VIP berhasil {message.text} ditambahkan 1 hari')
 		if db.get_vip_ends(int(message.text))[0] is None:
 			db.edit_vip_ends((datetime.now() + timedelta(days=1)).strftime('%d.%m.%Y %H:%M'), int(message.text))
            
@@ -522,7 +522,7 @@ async def buying_week(message):
 
 	try:
 		await bot.send_message(int(message.text), f'Durasi VIP berhasil ditambahkan 7 hari')
-		await bot.send_message(config.ADMINS, f'Durasi VIP berhasil ditambahkan 7 hari')
+		await bot.send_message(5458705482, f'Durasi VIP berhasil {message.text} ditambahkan 1 hari')
 		if db.get_vip_ends(int(message.text))[0] is None:
 			db.edit_vip_ends((datetime.now() + timedelta(days=1)).strftime('%d.%m.%Y %H:%M'), int(message.text))
            
@@ -553,7 +553,7 @@ async def buying_mounth(message):
 
 	try:
 		await bot.send_message(int(message.text), f'Durasi VIP berhasil ditambahkan 31 hari')
-		await bot.send_message(config.ADMINS, f'Durasi VIP berhasil ditambahkan 31 hari')
+		await bot.send_message(5458705482, f'Durasi VIP berhasil {message.text} ditambahkan 1 hari')
 		if db.get_vip_ends(int(message.text))[0] is None:
 			db.edit_vip_ends((datetime.now() + timedelta(days=31)).strftime('%d.%m.%Y %H:%M'), int(message.text))
            
