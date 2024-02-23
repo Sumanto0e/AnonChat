ae8e51213150f78a9f9e6267337815fba02c1d30
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-02-23 19:34:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -309,8 +309,8 @@ async def profile(message):
 			f'🔞 Usia: {db.get_age(user_id)[0]}\n\n'
 			f'👫 Jenis kelamin: {sex}\n\n'
 			f'🌍 Negara: {db.get_country(user_id)[0]}\n\n'
-			f'🏙️ Kota: {db.get_city(user_id)[0]}'
-			f'📍 Looking place: {db.get_city(user_id)[0]}',
+			f'🏙️ Kota: {db.get_city(user_id)[0]}\n\n'
+			f'📍 Looking place: {db.get_op_sex(user_id)[0]}',
    
 			reply_markup=kb.profile_kb)
 	except Exception as e:
