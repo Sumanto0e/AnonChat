183f139b748013c551e235bff747237bc65d401d
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2024-02-25 00:56:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -748,20 +748,22 @@ async def search_place(message):
 		if check_member.status not in ["member", "creator"]:
 			return await message.answer("<b>JOIN THE FIRST CHANNEL @ONSBASE AND DO IT Acak 🔀 AGAIN</b>", parse_mode='HTML')
 		user_id = message.from_user.id
+		if db.get_op_sex(user_id)[0] == 'None':
+			return await message.answer("set Looking place terlebih dahulu di sunting profil")
 		if db.get_vip_ends(message.from_user.id)[0] is not None and datetime.strptime(
 			db.get_vip_ends(message.from_user.id)[0], '%d.%m.%Y %H:%M') > datetime.now():
 			db.add_to_queue_vip(message.from_user.id, db.get_sex(message.from_user.id)[0], db.get_sex(user_id)[0])
-			await message.answer('Kami sedang mencari seseorang untuk anda.. 🔍', reply_markup=kb.cancel_search_kb)
+			await message.answer('Kami sedang mencari seseorang untuk anda.. 🔍\nBila lama coba untuk ganti looking place', reply_markup=kb.cancel_search_kb)
 			while True:
 				user_id = message.from_user.id
-				await asyncio.sleep(0.5)
-				if db.search_vip(message.from_user.id, db.get_sex(message.from_user.id)[0], db.get_sex(user_id))[0] is not None:
+				await asyncio.sleep(0.5)	
+				if db.search_vip(message.from_user.id, db.get_sex(message.from_user.id)[0], db.get_sex(user_id)[0]) is not None:
 					if db.get_sex(db.search(message.from_user.id)[0])[0] == db.get_sex(message.from_user.id)[0]:
 							db.update_connect_with(
 								db.search(message.from_user.id)[0], message.from_user.id)
 							db.update_connect_with(
 								message.from_user.id, db.search(message.from_user.id)[0])
-							break						
+							break
 			while True:
 				await asyncio.sleep(0.5)
 				if db.get_connect_with(message.from_user.id)[0] is not None:
@@ -787,7 +789,7 @@ async def search_place(message):
 					                    reply_markup=kb.stop_kb)
 			else:
 				await bot.send_message(message.from_user.id, 'Menemukan seseorang untukmu 💕', reply_markup=kb.stop_kb)
-			if db.get_vip_ends(db.get_connect_with(message.from_user.id)[0])[0] is not None and datetime.strptime(
+			if db.get_vip_ends(db.get_connect_with(message.from_user.id)[0][0]) is not None and datetime.strptime(
 				db.get_vip_ends(db.get_connect_with(message.from_user.id)[0])[0],
 				'%d.%m.%Y %H:%M') > datetime.now():
 				sex = 'Tidak dikenal'
@@ -799,7 +801,7 @@ async def search_place(message):
 				text = f'Menemukan seseorang untukmu 💕\n🅰️ Nama: {db.get_name(user_id)[0]}\n🔞 Usia: {db.get_age(user_id)[0]}\n👫 Jenis kelamin: {sex}\n🌍 Negara: {db.get_country(user_id)[0]}\n🏙️ Kota: {db.get_city(user_id)[0]}\n👍: {db.get_likes(user_id)[0]} 👎: {db.get_dislikes(user_id)[0]}'
 				profile_pictures = await dp.bot.get_user_profile_photos(user_id)
 				await bot.send_photo(db.get_connect_with(message.from_user.id)[0], (dict((profile_pictures.photos[0][0])).get("file_id")), caption=text,
-				                       reply_markup=kb.stop_kb)
+					                       reply_markup=kb.stop_kb)
 			else:
 				await bot.send_message(db.get_connect_with(message.from_user.id)[0], 'Menemukan seseorang untukmu 💕',
 				                       reply_markup=kb.stop_kb)
